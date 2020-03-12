01d0fa7c99fa9068bb756d0a95905b83de8bef21
lrichcard <lrichcard@gmail.com>
2020-03-11 21:48:49 -0400

    configuring the land Scape mode

--- app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
+++ app/src/main/java/com/example/flixster/adapters/MovieAdapter.java
@@ -1,6 +1,7 @@
 package com.example.flixster.adapters;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -63,7 +64,20 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder>{
         public void bind(Movie movie) {
             tvTitle.setText(movie.getTitle());
             tvOverview.setText(movie.getOverview());
-            Glide.with(context).load(movie.getPosterPath()).into(ivPoster);
+            String imageUrl;
+
+            //if phone is is in landScape
+            if(context.getResources().getConfiguration().orientation== Configuration.ORIENTATION_LANDSCAPE) {
+                //if then imageUrl = back drop image
+
+                imageUrl = movie.getBackdropPath();
+            }
+            else{
+                // else imageUrl = poster image
+                imageUrl = movie.getPosterPath();
+            }
+
+            Glide.with(context).load(imageUrl).into(ivPoster);
         }
     }
 }
